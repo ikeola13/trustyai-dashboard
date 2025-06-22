b9b61e72d8e55a2cf811eab294383a4660c4d432
Emmanuel Ikeola <eikeola@redhat.com>
2025-06-22 16:06:42 +0100

    Set up Cypress test

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'cypress';
+import cypressGrep from '@cypress/grep/src/plugin';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:8080',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 30000,
+    requestTimeout: 30000,
+    responseTimeout: 30000,
+    pageLoadTimeout: 30000,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      cypressGrep(config);
+      return config;
+    },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
+  },
+});
